7bec9371dcdf6a71423d32bf20164da74ef66624
Sameer Shekhar <shekharsameer2308@gmail.com>
2026-09-02 00:21:47 +0530

    chore: finalized root cleanup and formatting updates

--- src/emethanol/reactor.py
+++ src/emethanol/reactor.py
@@ -85,7 +85,12 @@ class MembraneReactor1D:
             * (1 - (p_MeOH * p_H2O) / (Keq1 * p_CO2 * (p_H2**3)))
             / (denom**3)
         )
-        r_rwgs = ((k1 * 1e-4) * p_CO2 * p_H2 * (1 - (p_CO * p_H2O) / (Keq3 * p_CO2 * p_H2)) / denom
+        r_rwgs = (
+            (k1 * 1e-4)
+            * p_CO2
+            * p_H2
+            * (1 - (p_CO * p_H2O) / (Keq3 * p_CO2 * p_H2))
+            / denom
         )
 
         return float(r_meoh), float(r_rwgs)
